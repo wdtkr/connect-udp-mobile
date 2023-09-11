8b752854a0fb9445c2351486305dc04bb72b8ae1
わだたける <68636852+wdtkr@users.noreply.github.com>
2023-09-11 19:51:43 +0900

    kotlinにライブラリをいったん追加

--- Kotlin/UdpConnect/app/src/main/java/com/example/udpconnect/MainActivity.kt
+++ Kotlin/UdpConnect/app/src/main/java/com/example/udpconnect/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.udpconnect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.udpconnect.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var receiveMessage = "受信したメッセージ"
+    private var stateText = "受信開始前です"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    fun onClickSendButton(){
+
+    }
+
+    fun onClickStartReceiveButton(){
+
+    }
+
+    fun onClickEndReceiveButton(){
+
     }
 }
